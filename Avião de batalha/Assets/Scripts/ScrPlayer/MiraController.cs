089b3f675b5c1cb00e9b1e7fddf583d593afb6dc
Gustavo Lima dos Santos <gugolho@gmail.com>
2022-06-23 15:58:42 -0300

    Fiz a torreta
    Agora a torreta atira na direção do mouse

--- Avião de batalha/Assets/Scripts/ScrPlayer/MiraController.cs
+++ Avião de batalha/Assets/Scripts/ScrPlayer/MiraController.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class MiraController : MonoBehaviour
 {
-    private Rigidbody2D meuRB;
+    
+    [SerializeField] private Transform pointer;
 
     // Start is called before the first frame update
     void Start()
     {
-        meuRB = GetComponent<Rigidbody2D>();
-
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -23,8 +22,8 @@ public class MiraController : MonoBehaviour
     private void LocalMouse()
     {
         Vector3 mousePos = Input.mousePosition;
-        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        mousePos.z = 0.5f;
 
-        meuRB.transform.position = new Vector3(mousePos.x, mousePos.y, 0f);
+        pointer.position = Camera.main.ScreenToWorldPoint(mousePos);
     }
 }
--- Avião de batalha/Assets/Scripts/ScrPlayer/PlayerController.cs
+++ Avião de batalha/Assets/Scripts/ScrPlayer/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D meuRB;
 
     [SerializeField] private float vel = 3f;
+    [SerializeField] private float velTiro = 7f;
+    [SerializeField] private GameObject Bala;
 
     // Start is called before the first frame update
     void Start()
@@ -39,5 +41,27 @@ public class PlayerController : MonoBehaviour
         }
 
 
+    }
+    //Atirando só se estou visivel
+    protected void Atirando()
+    {
+        
+        //Atirando
+            
+        var tiro1 = Instantiate(Bala, transform.position, transform.rotation);
+
+        //Encontrando o valor da direção que é pra ir
+        //pegando a direção que é pra ir
+        Vector2  dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //Normalizando a velocidade dele
+        dir.Normalize();
+
+        tiro1.transform.eulerAngles = new Vector3(0f, 0f, angle);
+
+        //Dando a direção e velocidade pro tiro
+        tiro1.GetComponent<Rigidbody2D>().velocity = dir * velTiro;
+
+        Debug.DrawRay(tiro1.transform.position, dir, Color.red, 5f);
     }
 }
